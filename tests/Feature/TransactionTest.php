a1c9d4df8ef6aee10123c5fe19177c692e51c755
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-04-10 20:44:45 -0300

    feat: add teste for credit and debit

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -39,4 +39,58 @@ class TransactionTest extends TestCase
         ]);
     }
 
+    public function test_credit_card_transaction_a_successful_response(): void
+    {
+        $account = Account::factory()->create();
+        $faker = Faker::create();
+        $transactionValue = $faker->randomFloat(2, 0, $account->saldo - 1);
+        $data = [
+            'forma_pagamento' => 'C',
+            'conta_id' => $account->conta_id,
+            'valor' =>  $transactionValue
+        ];
+        $finalAmount = $transactionValue - ($transactionValue * 0.05);
+        $newBalanceValidation = number_format($account->saldo - $finalAmount, 2);
+
+        $response = $this->postJson('/api/transacao', $data);
+
+        $response->assertStatus(200)
+        ->assertJson([
+            'conta_id' => $data['conta_id'],
+            'saldo' => $newBalanceValidation
+        ]);
+
+        $this->assertDatabaseHas('accounts', [
+            'conta_id' => $data['conta_id'],
+            'saldo' => $newBalanceValidation
+        ]);
+    }
+
+    public function test_debit_card_transaction_a_successful_response(): void
+    {
+        $account = Account::factory()->create();
+        $faker = Faker::create();
+        $transactionValue = $faker->randomFloat(2, 0, $account->saldo - 1);
+        $data = [
+            'forma_pagamento' => 'D',
+            'conta_id' => $account->conta_id,
+            'valor' =>  $transactionValue
+        ];
+        $finalAmount = $transactionValue - ($transactionValue * 0.03);
+        $newBalanceValidation = number_format($account->saldo - $finalAmount, 2);
+
+        $response = $this->postJson('/api/transacao', $data);
+
+        $response->assertStatus(200)
+        ->assertJson([
+            'conta_id' => $data['conta_id'],
+            'saldo' => $newBalanceValidation
+        ]);
+
+        $this->assertDatabaseHas('accounts', [
+            'conta_id' => $data['conta_id'],
+            'saldo' => $newBalanceValidation
+        ]);
+    }
+
 }
