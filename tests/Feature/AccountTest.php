7a69757b09c7aad0ffcb8339437d17d311f9ffbc
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-04-10 16:13:32 -0300

    feat: add test negative balance

--- tests/Feature/AccountTest.php
+++ tests/Feature/AccountTest.php
@@ -56,4 +56,18 @@ class AccountTest extends TestCase
 
     }
 
+    public function test_create_account_with_a_negative_balance_error_response(): void
+    {
+        $faker = Faker::create();
+        $data = [
+            'conta_id' => $faker->randomNumber(5),
+            'valor' =>  $faker->randomFloat(2, -1000, 1000)
+        ];
+        $response = $this->postJson('/api/conta', $data);
+
+        $response->assertStatus(422)
+        ->assertJsonStructure(['errors' => ['valor']]);
+        
+    }
+
 }
