ba8dfd109f9d6c08ffb7e0b1ecc1c10472219183
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-04-10 20:07:20 -0300

    feat: add a transaction test

---
+++ tests/Feature/TransactionTest .php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Faker\Factory as Faker;
+use App\Models\Account;
+use Database\Factories\AccountFactory;
+
+
+class TransactionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_pix_transaction_a_successful_response(): void
+    {
+        $account = Account::factory()->create();
+        $faker = Faker::create();
+        $transactionValue = $faker->randomFloat(2, 0, $account->saldo - 1);
+        $data = [
+            'forma_pagamento' => 'P',
+            'conta_id' => $account->conta_id,
+            'valor' =>  $transactionValue
+        ];
+        $newBalanceValidation = $account->saldo - $newValue;
+
+        $response = $this->postJson('/api/transacao', $data);
+
+        $response->assertStatus(201)
+        ->assertJson([
+            'conta_id' => $data['conta_id'],
+            'saldo' => $newBalanceValidation
+        ]);
+
+        $this->assertDatabaseHas('accounts', [
+            'conta_id' => $data['conta_id'],
+            'saldo' => $newBalanceValidation
+        ]);
+    }
+
+}
