333403f935a3acf3e3df6d3716de0194845c305a
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-04-10 13:47:25 -0300

    feat: add initial test file

---
+++ tests/Feature/AccountTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Faker\Factory as Faker;
+
+
+class AccountTest extends TestCase
+{
+
+    public function test_create_account_a_successful_response(): void
+    {
+        $faker = Faker::create();
+        $data = [
+            'conta_id' => $faker->randomNumber(5),
+            'valor' => $faker->randomFloat(2, 0, 1000)
+        ];
+        $response = $this->postJson('/api/conta', $data);
+
+        $response->assertStatus(201)
+        ->assertJsonStructure([
+            'conta_id',
+            'saldo'
+         ]);
+    }
+
+}
