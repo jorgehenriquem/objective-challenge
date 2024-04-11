2d2e99dd43963c582c428b50bdd9f47f6f60b3cf
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-04-10 23:27:20 -0300

    fix: remove AccountFactory

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -6,9 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Faker\Factory as Faker;
 use App\Models\Account;
-use Database\Factories\AccountFactory;
 use App\Enums\PaymentMethod;
-
 
 class TransactionTest extends TestCase
 {
