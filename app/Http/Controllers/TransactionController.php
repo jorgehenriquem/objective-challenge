818f68d0293ca21f9b420b4576f4c7cfedd6b647
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-04-10 23:28:47 -0300

    fix: remove Validator

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\TransactionService;
-use Illuminate\Support\Facades\Validator;
-
 
 class TransactionController extends Controller
 {
