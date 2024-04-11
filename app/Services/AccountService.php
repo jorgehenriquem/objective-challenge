6a77042b053f89232bab5870c852391d46896f3d
Jorge Henrique <jorge.henriquem@hotmail.com>
2024-04-10 23:14:17 -0300

    docs: add phpdoc in Account

--- app/Services/AccountService.php
+++ app/Services/AccountService.php
@@ -6,6 +6,13 @@ use App\Models\Account;
 
 class AccountService
 {
+    /**
+     * Creates a new account with the provided account ID and balance.
+     *
+     * @param int $conta_id The ID of the account.
+     * @param float $valor The initial balance of the account.
+     * @return \App\Models\Account The newly created account.
+     */
     public function createAccountWithBalance($conta_id, $valor)
     {
 
@@ -16,7 +23,12 @@ class AccountService
 
         return $account;
     }
-
+    /**
+     * Finds and returns an account by its ID.
+     * 
+     * @param int $id The ID of the account to find.
+     * @return \App\Models\Account|null The found account, or null if not found.
+     */
     public function findAccount($id)
     {
         return Account::find($id);
